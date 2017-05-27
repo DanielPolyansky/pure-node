d23c3e98ef2424e2058ef8733a8e941efda9cccf
DanielPolyansky <daniyilpolyanskiy@gmail.com>
2017-05-27 21:24:04 +0300

    adding express

--- server.js
+++ server.js
@@ -1,45 +1,14 @@
-const http = require('http');
-const host = '127.1.0.0';
-const port = '3000';
-const qrImage = require('qr-image');
-const fs = require('fs'); //file system , not file stream
+const express = require('express');
+const app = express();
+const port = 3000;
 const path = require('path');
-const mimes = {
-    ".htm": "text/html",
-    ".css": "text/css",
-    ".js": "text/javascript",
-    ".gif": "image/gif",
-    ".jpg": "image/jpg",
-    ".png": "image/png"
-}
+const bodyParser = require('body-parser');
 
-qrImage
-    .image('http://www.nodejs.org', { type: 'png', size: 20 }).pipe(fs.createWriteStream('MyQR.png'));
-const server = http.createServer((req, res) => {
-    const filepath = (req.url === '/') ? ('./index.html') : ('.' + req.url);
-    const contentType = mimes[path.extname(filepath)];
-    fs.exists(filepath, (file_exists) => {
-        if (file_exists) {
-            /*fs.readFile(filepath, (err, content) => {
-                if (err) {
-                    res.writeHead(500);
-                    res.end();
-                } else {
-                    res.writeHead(200, { 'Content-Type': contentType });
-                    res.end(content, 'utf-8');
-                }
-            });*/
-            res.writeHead(200, { 'Content-Type': contentType });
-            const streamFile = fs.createReadStream(filepath).pipe(res);
-            streamFile.on('err', () => {
-                writeHead(500);
-                res.end();
-            })
-        } else {
-            res.writeHead(404);
-            res.end('sorry! file not found');
-        }
-    });
-}).listen(port, host, () => {
-    console.log('server is running on port 3000');
+app.use(bodyParser.json());
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log('server running on port ' + port);
 });
