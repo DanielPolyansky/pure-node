ccb9ac7495050e29d9b915ad9c4f7d36a2dce707
DanielPolyansky <daniyilpolyanskiy@gmail.com>
2017-05-27 22:07:06 +0300

    adding routes

--- server/server.js
+++ server/server.js
@@ -3,11 +3,10 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const router = require('./api/routes');
 app.use(bodyParser.json());
-app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', router);
 
 app.listen(port, () => {
     console.log('server running on port ' + port);
