48b4873ffa70bba386de629301f8a51b1dcb812f
DanielPolyansky <daniyilpolyanskiy@gmail.com>
2017-05-30 00:35:48 +0300

    token verification

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const morgan = require('morgan');
 const validator = require('express-validator');
-
+const apiRoutes = require('express').Router();
 mongoose.Promise = global.Promise;
 
 mongoose.connect(database, (err) => {
@@ -26,10 +26,35 @@ app.set('superSecret', secret);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(validator());
+
+apiRoutes.use(function(req, res, next) {
+
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    if (token) {
+
+        jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+});
+
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(morgan('dev'));
 app.use('/', router);
-
+app.use('/api', apiRoutes);
 app.listen(port, () => {
     console.log('server running on port ' + port);
 });
