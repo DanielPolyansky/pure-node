f629df2827e44e0836d064e00b6873bc3e1fab31
DanielPolyansky <daniyilpolyanskiy@gmail.com>
2017-06-05 18:13:13 +0300

    starting with geolocation

---
+++ server/api/geolocation.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDHTIPj1Oj_Zk7LTUQ-noopkGg6OeU8_uo',
+    json: true
+}, (err, res, body) => {
+    console.log(body);
+});
