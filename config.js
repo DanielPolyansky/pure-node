a51fe40d36bdb3b6c32b61e30c7c3371e9afd951
DanielPolyansky <daniyilpolyanskiy@gmail.com>
2017-06-11 13:07:29 +0300

    port final I hope

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ module.exports = {
 
     'secret': 'EleksProject',
     'database': 'mongodb://test:test@ds129641.mlab.com:29641/ekeks-project',
-    'port': 3000 || process.env.PORT
-
+    'port': process.env.PORT || 3000
 };
